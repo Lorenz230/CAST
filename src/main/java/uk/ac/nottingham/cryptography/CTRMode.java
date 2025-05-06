8efbfb42e54f7394a280ad2c85e9e04c77a826cd
Lorenz230 <lorenzcamento0@gmail.com>
2025-05-06 17:33:56 +0100

    CTR is now also working perfectly.

--- src/main/java/uk/ac/nottingham/cryptography/CTRMode.java
+++ src/main/java/uk/ac/nottingham/cryptography/CTRMode.java
@@ -9,6 +9,12 @@ package uk.ac.nottingham.cryptography;
  */
 public class CTRMode extends CipherMode {
 
+    private Cipher cipher;
+    private byte[] nonce;
+    private long counter;
+    private byte[] keystream = new byte[24];
+    private int keystreamIndex = 24;
+
     public CTRMode() {
         super();
     }
@@ -16,20 +22,57 @@ public class CTRMode extends CipherMode {
     @Override
     public void initialise(Cipher cipher, byte[] key, byte[] nonce) {
         // Add your code here
+        this.cipher = cipher;
+        this.nonce = nonce.clone(); // defensive copy
+        this.counter = 0;
+        this.keystreamIndex = 24; // start with empty keystream
+        cipher.initialise(key);
     }
 
     @Override
     public void encrypt(byte[] data) {
         // Add your code here
+        for (int i = 0; i < data.length; i++) {
+            if (keystreamIndex == 24) {
+                byte[] inputBlock = new byte[24];
+                System.arraycopy(nonce, 0, inputBlock, 0, 16);
+
+                for (int j = 0; j < 8; j++) {
+                    inputBlock[16 + j] = (byte) ((counter >>> (56 - 8 * j)) & 0xFF);
+                }
+
+                cipher.encrypt(inputBlock);
+                System.arraycopy(inputBlock, 0, keystream, 0, 24);
+                keystreamIndex = 0;
+                counter++;
+            }
+
+            data[i] ^= keystream[keystreamIndex++];
+        }
     }
 
     @Override
     public void decrypt(byte[] data) {
         // Add your code here;
+        encrypt(data); // symmetric under XOR
     }
+
 
     @Override
     public void seek(byte[] counter) {
         // Add your code here
+        long value = 0;
+        int padding = 8 - counter.length;
+
+        for (int i = 0; i < padding; i++) {
+            value <<= 8;
+        }
+
+        for (byte b : counter) {
+            value = (value << 8) | (b & 0xFF);
+        }
+
+        this.counter = value;
+        this.keystreamIndex = 24; // invalidate current keystream
     }
 }
